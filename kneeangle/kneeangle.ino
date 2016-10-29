62ea5fefd0d637c79177fe28852ddd7ef27b35cb
MBIT-Daniel-Wallner <daniel.wallner@mbit.at>
2016-10-29 20:18:39 +0200

    without interrupts

--- kneeangle/kneeangle.ino
+++ kneeangle/kneeangle.ino
@@ -41,6 +41,11 @@
 #include "MPU6050_6Axis_MotionApps20.h"
 //#include "MPU6050.h"
 
+#define dt 0.01
+#define MAX_VAL 32767
+#define MIN_VAL -32768
+//#define ENABLE_CALIBRATION
+
 ///////////////////////////////////   CONFIGURATION   /////////////////////////////
 //Change this 3 variables if you want to fine tune the skecth to your needs.
 int buffersize = 1000;   //Amount of readings used to average, make it higher to get more precision but sketch will be slower  (default:1000)
@@ -55,32 +60,14 @@ int giro_deadzone = 2;   //Giro error allowed, make it lower to get more precisi
 MPU6050 mpu1(0x68);
 MPU6050 mpu2(0x69);
 
-int16_t ax1, ay1, az1;
-int16_t gx1, gy1, gz1;
-
-int16_t ax2, ay2, az2;
-int16_t gx2, gy2, gz2;
-
-int16_t angleX1, angleY1, angleZ1;
-int16_t angleX2, angleY2, angleZ2;
-
-int16_t corrX1, corrY1, corrZ1;
-int16_t corrX2, corrY2, corrZ2;
-
 Quaternion q1;
 Quaternion q2;
+Quaternion q;
 float euler1[3]; 
 float euler2[3]; 
 
-#define LED_PIN 13
-
-#define MPU1 7
-#define MPU2 8
-#define dt 0.01  //Sampling rate
-#define MAX_VAL 32767
-#define MIN_VAL -32768
-
 int phase = 0;
+unsigned long timer = millis();
 
 bool blinkState = false;
 
@@ -104,205 +91,9 @@ struct accelgyro_t {
   int interruptNumber;
   void (*interruptFunction)();
 } accelGyro1, accelGyro2;
-
-
-// ================================================================
-// ===               INTERRUPT DETECTION ROUTINE                ===
-// ================================================================
-
-volatile bool mpuInterrupt1 = false;     // indicates whether MPU interrupt pin has gone high
-void dmpDataReady1() {
-    mpuInterrupt1 = true;
-}
-
-volatile bool mpuInterrupt2 = false;     // indicates whether MPU interrupt pin has gone high
-void dmpDataReady2() {
-    mpuInterrupt2 = true;
-}
-
-void setup() {
-
-  accelGyro1.sensor = mpu1;
-  accelGyro1.interruptNumber = 0; 
-  accelGyro1.interruptFunction = dmpDataReady1;
-  accelGyro2.sensor = mpu2;
-  accelGyro2.interruptNumber = 1; 
-  accelGyro1.interruptFunction = dmpDataReady2;
-
-
-  // join I2C bus (I2Cdev library doesn't do this automatically)
-  Wire.begin();
-
-  // initialize serial communication
-  // (38400 chosen because it works as well at 8MHz as it does at 16MHz, but
-  // it's really up to you depending on your project)
-  Serial.begin(38400);
-
-  // initialize device
-  Serial.println("Initializing I2C devices...");
-  //accelgyro.initialize();
-  accelGyro1.sensor.initialize();
-  accelGyro2.sensor.initialize();
-
-  // verify connection
-  Serial.println("Testing device connections...");
-  Serial.println(accelGyro1.sensor.testConnection() ? "MPU6050 #1 connection successful" : "MPU6050 connection failed");
-  Serial.println(accelGyro2.sensor.testConnection() ? "MPU6050 #2 connection successful" : "MPU6050 connection failed");
-
-  // configure Arduino LED for
-  pinMode(LED_PIN, OUTPUT);
-}
-
-void loop() {
-  if (phase == 0) {
-    Serial.println("\nCalculating offsets and calibrating...");
-    calibration(accelGyro1.sensor);
-    calibration(accelGyro2.sensor);
-    phase++;
-    delay(1000);
-  }
-
-  if (phase == 1) {
-    Serial.println("\nCalibration done.");
-    Serial.println(F("Initializing DMP MPU6050 #1..."));
-    initializeDmp(&accelGyro1);
-    Serial.println(F("Initializing DMP MPU6050 #2..."));
-    initializeDmp(&accelGyro2);
-    phase++;
-    delay(300);
-  }
-
-  if (phase == 2) {
-    if (!(accelGyro1.dmpReady && accelGyro2.dmpReady)) return;
-    // wait for MPU interrupt or extra packet(s) available
-    while (!mpuInterrupt1 && !mpuInterrupt2 && accelGyro1.fifoCount < accelGyro1.packetSize && accelGyro2.fifoCount < accelGyro2.packetSize ) {
-        // other program behavior stuff here
-        // .
-        // .
-        // .
-        // if you are really paranoid you can frequently test in between other
-        // stuff to see if mpuInterrupt is true, and if so, "break;" from the
-        // while() loop to immediately process the MPU data
-        // .
-        // .
-        // .
-    }
-    // reset interrupt flag and get INT_STATUS byte
-    mpuInterrupt1 = false;
-    mpuInterrupt2 = false;
-    accelGyro1.mpuIntStatus = accelGyro1.sensor.getIntStatus();
-    accelGyro2.mpuIntStatus = accelGyro2.sensor.getIntStatus();
-
-    // get current FIFO count
-    accelGyro1.fifoCount = accelGyro1.sensor.getFIFOCount();
-    accelGyro2.fifoCount = accelGyro2.sensor.getFIFOCount();
-
-    // check for overflow (this should never happen unless our code is too inefficient)
-    if ((accelGyro1.mpuIntStatus & 0x10) ||(accelGyro2.mpuIntStatus & 0x10) || accelGyro1.fifoCount == 1024 || accelGyro2.fifoCount == 1024) {
-        // reset so we can continue cleanly
-        accelGyro1.sensor.resetFIFO();
-        accelGyro2.sensor.resetFIFO();
-        Serial.println(F("FIFO overflow!"));
-
-    // otherwise, check for DMP data ready interrupt (this should happen frequently)
-    } else if (accelGyro1.mpuIntStatus & 0x02 && accelGyro2.mpuIntStatus & 0x02) {
-        // wait for correct available data length, should be a VERY short wait
-        while (accelGyro1.fifoCount < accelGyro1.packetSize) accelGyro1.fifoCount = accelGyro1.sensor.getFIFOCount();
-        while (accelGyro2.fifoCount < accelGyro2.packetSize) accelGyro2.fifoCount = accelGyro2.sensor.getFIFOCount();
-
-        // read a packet from FIFO
-        accelGyro1.sensor.getFIFOBytes(accelGyro1.fifoBuffer, accelGyro1.packetSize);
-        accelGyro2.sensor.getFIFOBytes(accelGyro2.fifoBuffer, accelGyro2.packetSize);
-        
-        // track FIFO count here in case there is > 1 packet available
-        // (this lets us immediately read more without waiting for an interrupt)
-        accelGyro1.fifoCount -= accelGyro1.packetSize;
-        accelGyro2.fifoCount -= accelGyro2.packetSize;
-
-
-        accelGyro1.sensor.dmpGetQuaternion(&q1, accelGyro1.fifoBuffer);
-        accelGyro1.sensor.dmpGetEuler(euler1, &q1);
-        Serial.print("euler sensor 1\t");
-        Serial.print(euler1[0] * 180 / M_PI);
-        Serial.print("\t");
-        Serial.print(euler1[1] * 180 / M_PI);
-        Serial.print("\t");
-        Serial.println(euler1[2] * 180 / M_PI);
-    
-        accelGyro2.sensor.dmpGetQuaternion(&q2, accelGyro2.fifoBuffer);
-        accelGyro2.sensor.dmpGetEuler(euler2, &q2);
-        Serial.print("euler sensor 2\t");
-        Serial.print(euler2[0] * 180 / M_PI);
-        Serial.print("\t");
-        Serial.print(euler2[1] * 180 / M_PI);
-        Serial.print("\t");
-        Serial.println(euler2[2] * 180 / M_PI);
-    }
-  }
-
-  if (phase == 3) {
-    // read raw accel/gyro measurements from device
-    accelGyro1.sensor.getMotion6(&ax1, &ay1, &az1, &gx1, &gy1, &gz1);
-    accelGyro2.sensor.getMotion6(&ax2, &ay2, &az2, &gx2, &gy2, &gz2);
-
-    // these methods (and a few others) are also available
-    //accelgyro.getAcceleration(&ax, &ay, &az);
-    //accelgyro.getRotation(&gx, &gy, &gz);
-
-    corrX1 = 0.98 * (corrX1 + gx1 * dt) + 0.02 * (ax1); //Complimentary filter for MPU6050 sensor 1
-    corrY1 = 0.98 * (corrY1 + gy1 * dt) + 0.02 * (ay1);
-    corrZ1 = 0.98 * (corrZ1 + gz1 * dt) + 0.02 * (az1);
-
-    corrX2 = 0.98 * (corrX2 + gx2 * dt) + 0.02 * (ax2); //Complimentary filter for MPU6050 sensor 1
-    corrY2 = 0.98 * (corrY2 + gy2 * dt) + 0.02 * (ay2);
-    corrZ2 = 0.98 * (corrZ2 + gz2 * dt) + 0.02 * (az2);
-
-    angleX1 = readValueToAngle(corrX1);
-    angleY1 = readValueToAngle(corrY1);
-    angleZ1 = readValueToAngle(corrZ1);
-
-    angleX2 = readValueToAngle(corrX2);
-    angleY2 = readValueToAngle(corrY2);
-    angleZ2 = readValueToAngle(corrZ2);
-
-    // display tab-separated accel/gyro x/y/z values
-    Serial.print("MPU1:\t");
-    Serial.print(ax1); Serial.print("\t");
-    Serial.print(ay1); Serial.print("\t");
-    Serial.print(az1); Serial.print("\t");
-    Serial.print(gx1); Serial.print("\t");
-    Serial.print(gy1); Serial.print("\t");
-    Serial.print(gz1); Serial.print("\t");
-    Serial.print(angleX1); Serial.print("\t");
-    Serial.print(angleY1); Serial.print("\t");
-    Serial.println(angleZ1);
-
-
-    // display tab-separated accel/gyro x/y/z values
-    Serial.print("MPU2:\t");
-    Serial.print(ax2); Serial.print("\t");
-    Serial.print(ay2); Serial.print("\t");
-    Serial.print(az2); Serial.print("\t");
-    Serial.print(gx2); Serial.print("\t");
-    Serial.print(gy2); Serial.print("\t");
-    Serial.print(gz2); Serial.print("\t");
-    Serial.print(angleX2); Serial.print("\t");
-    Serial.print(angleY2); Serial.print("\t");
-    Serial.println(angleZ2);
-
-
-    // blink LED to indicate activity
-    blinkState = !blinkState;
-    digitalWrite(LED_PIN, blinkState);
-  }
-
-}
-
-int readValueToAngle(int16_t input) {
-  return map(input, MIN_VAL, MAX_VAL, 0, 180);
-}
-
 ///////////////////////////////////   FUNCTIONS   ////////////////////////////////////
+
+#ifdef ENABLE_CALIBRATION
 void calibration(MPU6050 accelgyro) {
   int ax_offset, ay_offset, az_offset, gx_offset, gy_offset, gz_offset;
   accelgyro_values_t mean_val = meansensors(accelgyro);
@@ -409,6 +200,7 @@ accelgyro_values_t meansensors(MPU6050 accelgyro) {
   }
   return mean_val;
 }
+#endif
 
 int initializeDmp(accelgyro_t* accelgyro) {
   int devStatus = accelgyro->sensor.dmpInitialize();
@@ -421,7 +213,7 @@ int initializeDmp(accelgyro_t* accelgyro) {
 
     // enable Arduino interrupt detection
     Serial.println(F("Enabling interrupt detection (Arduino external interrupt)..."));
-    attachInterrupt(accelgyro->interruptNumber, accelgyro->interruptFunction, RISING);
+    //attachInterrupt(accelgyro->interruptNumber, accelgyro->interruptFunction, RISING);
     accelgyro->mpuIntStatus = accelgyro->sensor.getIntStatus();
 
     // set our DMP Ready flag so the main loop() function knows it's okay to use it
@@ -442,6 +234,141 @@ int initializeDmp(accelgyro_t* accelgyro) {
     return false;
   }
 }
+int tryReadLastQuaternion(accelgyro_t* accelGyro){
+  
+  accelGyro->mpuIntStatus = accelGyro->sensor.getIntStatus();
+  // get current FIFO count
+  accelGyro->fifoCount = accelGyro->sensor.getFIFOCount();
+
+  // check for overflow (this should never happen unless our code is too inefficient)
+  if ((accelGyro->mpuIntStatus & 0x10) || accelGyro->fifoCount == 1024 ) {
+      // reset so we can continue cleanly
+      accelGyro->sensor.resetFIFO();
+      Serial.println(F("FIFO overflow!"));
+  }else if (accelGyro->mpuIntStatus & 0x02) {
+      while (accelGyro->fifoCount < accelGyro->packetSize) {
+        accelGyro->fifoCount = accelGyro->sensor.getFIFOCount();
+      }
+      accelGyro->sensor.getFIFOBytes(accelGyro->fifoBuffer, accelGyro->packetSize);
+      accelGyro->sensor.dmpGetQuaternion(&q, accelGyro->fifoBuffer);
+      accelGyro->sensor.resetFIFO();
+      accelGyro->fifoCount = 0;
+      return true;
+  }
+  return false;
+}
+
+void setOffsets(MPU6050 accelgyro, int ax_offset, int ay_offset, int az_offset, int gx_offset, int gy_offset, int gz_offset){
+    accelgyro.setXAccelOffset(ax_offset);
+    accelgyro.setYAccelOffset(ay_offset);
+    accelgyro.setZAccelOffset(az_offset);
+
+    accelgyro.setXGyroOffset(gx_offset);
+    accelgyro.setYGyroOffset(gy_offset);
+    accelgyro.setZGyroOffset(gz_offset);
+}
+
+double calculateAngle(Quaternion q1, Quaternion q2){
+  q1.normalize();
+  q2.normalize();
+  return acos(dotProductQuaternions(q1,q2)) / PI * 180;
+}
+
+double dotProductQuaternions(Quaternion left, Quaternion right) {
+  return left.x * right.x + left.y * right.y + left.z * right.z + left.w * right.w;
+}
 
 
+void setup() {
 
+  accelGyro1.sensor = mpu1;
+  accelGyro2.sensor = mpu2;
+
+  // join I2C bus (I2Cdev library doesn't do this automatically)
+  Wire.begin();
+
+  // initialize serial communication
+  // (38400 chosen because it works as well at 8MHz as it does at 16MHz, but
+  // it's really up to you depending on your project)
+  Serial.begin(38400);
+
+  // initialize device
+  Serial.println("Initializing I2C devices...");
+  //accelgyro.initialize();
+  accelGyro1.sensor.initialize();
+  accelGyro2.sensor.initialize();
+
+  // verify connection
+  Serial.println("Testing device connections...");
+  Serial.println(accelGyro1.sensor.testConnection() ? "MPU6050 #1 connection successful" : "MPU6050 connection failed");
+  Serial.println(accelGyro2.sensor.testConnection() ? "MPU6050 #2 connection successful" : "MPU6050 connection failed");
+
+}
+
+void loop() {
+  int res = false;
+  int newAngle = false;
+  
+  if (phase == 0) {
+    Serial.println("\nCalculating offsets and calibrating...");
+    #ifdef ENABLE_CALIBRATION
+    calibration(accelGyro1.sensor);
+    calibration(accelGyro2.sensor);
+    #else
+    setOffsets(accelGyro1.sensor, 1422,1897,870,80,50,59);
+    setOffsets(accelGyro2.sensor, 927,-1049,586,234,-11,-23);
+    #endif
+    phase++;
+    delay(1000);
+  }
+
+  if (phase == 1) {
+    Serial.println("\nCalibration done.");
+    Serial.println(F("Initializing DMP MPU6050 #1..."));
+    initializeDmp(&accelGyro1);
+    Serial.println(F("Initializing DMP MPU6050 #2..."));
+    initializeDmp(&accelGyro2);
+    phase++;
+    delay(300);
+    accelGyro1.sensor.resetFIFO();
+    accelGyro2.sensor.resetFIFO();
+  }
+
+  if (phase == 2) {
+    
+    if (!(accelGyro1.dmpReady && accelGyro2.dmpReady )) return;
+      res = tryReadLastQuaternion(&accelGyro1);
+      if(res) {
+        q1 = q;   
+        newAngle = true;
+      }
+      res = tryReadLastQuaternion(&accelGyro2);
+      if(res){
+        q2 = q;
+        newAngle = true;
+      }
+
+      if(newAngle && (millis()-timer > 500)){
+        Serial.print("quat1\t");
+        Serial.print(q1.w);
+        Serial.print("\t");
+        Serial.print(q1.x);
+        Serial.print("\t");
+        Serial.print(q1.y);
+        Serial.print("\t");
+        Serial.println(q1.z);
+        Serial.print("quat2\t");
+        Serial.print(q2.w);
+        Serial.print("\t");
+        Serial.print(q2.x);
+        Serial.print("\t");
+        Serial.print(q2.y);
+        Serial.print("\t");
+        Serial.println(q2.z);
+        Serial.print("Angle: ");
+        Serial.println(calculateAngle(q1,q2));
+        newAngle = false;
+        timer = millis();
+      }
+  }
+}
